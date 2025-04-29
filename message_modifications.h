eafcb8ea5d72483b00c7cd69c69061a8b9e1a78d
gldenf <farisadouglas58@gmail.com>
2025-04-29 00:56:11 -0400

    message_modifications.h

--- message_modifications.h
+++ message_modifications.h
@@ -1,1 +1,8 @@
+#ifndef PROCESSING_H_
+#define PROCESSING_H_
 
+char *redact(char *message, char *list);
+char *encrypt_columnar(char *message, char *key);
+char *decrypt_columnar(char *message, char *key);
+
+#endif
