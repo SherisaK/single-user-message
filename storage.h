e7d69031e551ade660eff586365a590cfdd1a640
gldenf <farisadouglas58@gmail.com>
2025-04-29 22:45:33 -0400

    Update storage.h

--- storage.h
+++ storage.h
@@ -1,14 +1,18 @@
+// This is the header file for the storage functions.
+// This file, defines the message structure and declares the various functions for saving and loading 
+//     messages from the file.
+
 #ifndef FILE_IO_H_
 #define FILE_IO_H_
 
-#define MAX_MESSAGES 100
+#define MAX_MESSAGES 100 
 #define MAX_TITLE_LENGTH 100
 #define MAX_MESSAGE_LENGTH 256
 
 typedef struct {
-    int id;
-    char title[MAX_TITLE_LENGTH];
-    char content[MAX_MESSAGE_LENGTH];
+    int id; // The unique identifier for the messages stored chronologically.
+    char title[MAX_TITLE_LENGTH]; // Title of the message.
+    char content[MAX_MESSAGE_LENGTH]; // Message Content.
 } Message;
 
 char *read_string(char *filename);
